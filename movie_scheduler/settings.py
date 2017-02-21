cc2a68992c9d0287f54eaa669aa4157d082fe83a
Yungon Park <hahafree12@gmail.com>
2017-02-21 19:55:31 +0900

    Scheduler core app 추가

--- movie_scheduler/settings.py
+++ movie_scheduler/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'scheduler_core.apps.SchedulerCoreConfig'
 ]
 
 MIDDLEWARE = [
