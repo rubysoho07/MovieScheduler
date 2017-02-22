269dbdcd08f3941a65ff4912a64a895fdc8c026c
Yungon Park <hahafree12@gmail.com>
2017-02-22 18:04:02 +0900

    Parser 수정 (classmethod -> staticmethod)

--- scheduler_core/movie_schedule_parser.py
+++ scheduler_core/movie_schedule_parser.py
@@ -14,8 +14,8 @@ class MovieScheduleParser(object):
         pass
 
     # Get original data from web.
-    @classmethod
-    def get_original_data(cls, url):
+    @staticmethod
+    def get_original_data(url):
         headers = {
             'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:49.0) Gecko/20100101 Firefox/49.0'
         }
