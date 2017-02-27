7e69586902ebe2efb741b46c25726d4a4b1a6ed7
Yungon Park <hahafree12@gmail.com>
2017-02-27 21:15:12 +0900

    영화 스케줄 표시 기본적인 부분 추가 (#3)

--- scheduler_core/views.py
+++ scheduler_core/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.views.generic.list import ListView
+
+from scheduler_core.models import MovieSchedule
+
 
 # Create your views here.
+class MovieScheduleListView(ListView):
+    model = MovieSchedule
