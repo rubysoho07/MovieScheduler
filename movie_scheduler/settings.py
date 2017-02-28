b5ed5153a9fba9cad4dbfcd007ff5d944152fa46
Yungon Park <hahafree12@gmail.com>
2017-02-28 16:46:13 +0900

    Heroku local에서 static 파일을 읽지 못하여 whitenoise 추가

--- movie_scheduler/settings.py
+++ movie_scheduler/settings.py
@@ -124,10 +124,10 @@ USE_TZ = False
 
 STATIC_URL = '/static/'
 
-if DEBUG:
-    STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
-else:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 # Celery
 CELERY_IMPORTS = ("scheduler_core.tasks",)
