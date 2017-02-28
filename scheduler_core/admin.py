4d209498b618eeaf81abc41a7903bbb16f018e4c
Yungon Park <hahafree12@gmail.com>
2017-02-28 16:45:08 +0900

    LatestUpdate admin page에 추가

--- scheduler_core/admin.py
+++ scheduler_core/admin.py
@@ -1,11 +1,17 @@
 from django.contrib import admin
 
-from .models import MovieSchedule
+from .models import MovieSchedule, LatestUpdate
 
 
 # MovieScheduleAdmin
 class MovieScheduleAdmin(admin.ModelAdmin):
     list_display = ("broadcast_company", "title", "start_time", "end_time", "ratings")
 
+
+# LatestUpdateAdmin
+class LatestUpdateAdmin(admin.ModelAdmin):
+    list_display = ("broadcast_company", "latest_update")
+
 # Register your models here.
 admin.site.register(MovieSchedule, MovieScheduleAdmin)
+admin.site.register(LatestUpdate, LatestUpdateAdmin)
