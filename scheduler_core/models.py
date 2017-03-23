10810f0c128fea7bfa440c4fdb5138a470ac4fcc
Yungon Park <hahafree12@gmail.com>
2017-03-23 09:05:45 +0900

    Model 수정

--- scheduler_core/models.py
+++ scheduler_core/models.py
@@ -20,14 +20,8 @@ class MovieSchedule(models.Model):
     end_time = models.DateTimeField()
     ratings = models.SmallIntegerField(blank=True, null=True)
 
-    def __unicode__(self):
-        return self.broadcast_company.bc_name + "/" + self.title
-
 
 # Latest update date.
 class LatestUpdate(models.Model):
     broadcast_company = models.ForeignKey('BroadcastCompany', on_delete=models.CASCADE)
     latest_update = models.DateTimeField(null=True)
-
-    def __unicode__(self):
-        return self.broadcast_company + "/" + str(self.latest_update)
