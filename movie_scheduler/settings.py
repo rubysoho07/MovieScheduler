2d915a8d4b5b6736c723f8d2740285ca6f5006dc
Yungon Park <hahafree12@gmail.com>
2017-04-04 14:36:32 +0900

    설정 파일 분리 (SECRET_KEY 및 민감한 설정 분리 목적)

--- movie_scheduler/settings.py
+++ movie_scheduler/settings.py
@@ -11,17 +11,29 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
+import json
+
 import dj_database_url
+
+from django.core.exceptions import ImproperlyConfigured
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+# Read JSON config file (From "Two Scoops of Django" book.)
+with open("settings.json") as f:
+    secrets = json.loads(f.read())
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '*gtv0!pn^l4ic-q_%m7uuoclh@ujmc&#1g--va4veo1!%mdso4'
+def get_secret(setting, secret=secrets):
+    """ Get secret variables, or return exception. """
+    try:
+        return secret[setting]
+    except KeyError:
+        error_msg = "Set the {0} environment variable".format(setting)
+        raise ImproperlyConfigured(error_msg)
+
+SECRET_KEY = get_secret('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
