9dbdc33979f139691e3359d78a7aa669e9ef49a8
Yungon Park <hahafree12@gmail.com>
2017-04-12 13:57:12 +0900

    Template directory 이동 및 404, 500 Error Page 추가

--- movie_scheduler/settings.py
+++ movie_scheduler/settings.py
@@ -52,7 +52,9 @@ ROOT_URLCONF = 'movie_scheduler.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
