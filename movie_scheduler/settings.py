982c753413a39933be19808e6b4faa45f4cdac45
Yungon Park <hahafree12@gmail.com>
2017-04-13 14:31:44 +0900

    collectstatic 명령이 수행되지 않아 설정 파일 수정

--- movie_scheduler/settings.py
+++ movie_scheduler/settings.py
@@ -122,7 +122,6 @@ USE_TZ = False
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
