242f9ae9c6de6616ebc1f8324ca08b8b4db87e7a
Yungon Park <hahafree12@gmail.com>
2017-07-23 17:50:05 +0900

    테스트 코드 개편 (#7)

--- scheduler_core/tests.py
+++ scheduler_core/tests.py
@@ -2,13 +2,71 @@
 from django.test import TestCase
 from django.db.models import Q
 from scheduler_core.models import BroadcastCompany, MovieSchedule
-from scheduler_core.movie_schedule_parser import CJScheduleParser, TCastScheduleParser
+from scheduler_core.movie_schedule_parser import MovieScheduleParser, CJScheduleParser, TCastScheduleParser
 from . import tasks
 from django.utils import timezone
 from bs4 import BeautifulSoup
 
 
 # Create your tests here.
+class MovieScheduleParserTestCase(TestCase):
+    """Test base class for movie schedule parser."""
+
+    def setUp(self):
+        """Config environments before testing."""
+        self.parser = MovieScheduleParser()
+        self.channel, _ = BroadcastCompany.objects.get_or_create(bc_name="OCN")
+        self.schedule_list = []
+
+    def test_get_original_data(self):
+        """Test to get BeautifulSoup object from schedule page."""
+        data = self.parser.get_original_data("http://ocn.tving.com/ocn/schedule")
+
+        self.assertNotEqual(data, None)
+
+    def test_parse_string_to_int(self):
+        """Test to parse string to integer."""
+        value = self.parser.parse_string_to_int("10", 1)
+        self.assertEqual(value, 10)
+
+        value2 = self.parser.parse_string_to_int("100")
+        self.assertEqual(value2, 100)
+
+        value3 = self.parser.parse_string_to_int("ABC")
+        self.assertEqual(value3, 0)
+
+    def test_make_schedule_object(self):
+        """Test to make MovieSchedule object with broadcast_company and dictionary for schedule."""
+        now_time = timezone.now()
+
+        schedule = {
+            "title": "Test Title",
+            "start_time": now_time,
+            "end_time": None,
+            "rating": 19
+        }
+
+        schedule_obj = self.parser.make_schedule_object(self.channel, schedule)
+
+        self.assertEqual(schedule_obj.title, schedule['title'])
+        self.assertEqual(schedule_obj.start_time, now_time)
+        self.assertEqual(schedule_obj.end_time, None)
+        self.assertEqual(schedule_obj.ratings, schedule['rating'])
+
+    def test_save_schedules(self):
+        """Test to save schedule from schedule list to database."""
+        schedule = {
+            "title": "Test Title",
+            "start_time": timezone.now(),
+            "end_time": None,
+            "rating": 19
+        }
+
+        self.schedule_list.append(schedule)
+
+        self.parser.save_schedule(self.channel, self.schedule_list)
+
+
 class CJScheduleTestCase(TestCase):
     """Test to get schedules from CJ E&M channel."""
 
@@ -59,7 +117,7 @@ class CJScheduleTestCase(TestCase):
         result = self.parser.parse_daily_schedule(self.schedule_table, self.schedule_date)
         self.assertEqual(len(result), 1)
 
-    def test_get_cj_schedule(self):
+    def test_get_cj_channel_schedule(self):
         """Test to get CJ E&M channel schedule"""
 
         date_str = timezone.datetime.strftime(self.schedule_date, "%Y%m%d")
@@ -78,37 +136,50 @@ class TCastScheduleTestCase(TestCase):
         """Configuration before testing"""
         self.channel, _ = BroadcastCompany.objects.get_or_create(bc_name="Screen")
         self.parser = TCastScheduleParser(self.channel)
-        self.parser_next_week = TCastScheduleParser(self.channel, timezone.datetime(2017, 4, 26))
+        self.original_data = """
+            <strong>21:35</strong>
+            <a href="#a" class="layerpopup1" onclick="aa">Test Schedule</a>
+            <img src="../images/common/icon_15age.gif" alt="Test Rating">
+        """.encode('utf-8')
+
+    def test_get_tcast_start_hour(self):
+        """Test to get start hour of t.cast channel."""
+        start_hour = self.parser.get_tcast_start_hour(self.channel)
+        self.assertEqual(start_hour, 5)
+
+        cinef_channel, _ = BroadcastCompany.objects.get_or_create(bc_name="Cinef")
+        start_hour = self.parser.get_tcast_start_hour(cinef_channel)
+        self.assertEqual(start_hour, 6)
+
+        test_channel, _ = BroadcastCompany.objects.get_or_create(bc_name="Test")
+        start_hour = self.parser.get_tcast_start_hour(test_channel)
+        self.assertEqual(start_hour, 5)
 
     def test_get_tcast_single_schedule(self):
         """Test to make single schedule"""
 
-        original_data = """
-        <strong>21:35</strong>
-        <a href="#a" class="layerpopup1" onclick="aa">Test Schedule</a>
-        <img src="../images/common/icon_15age.gif" alt="Test Rating">
-        """.encode('utf-8')
-        original_data_parsed = BeautifulSoup(original_data, 'html.parser')
+        original_data_parsed = BeautifulSoup(self.original_data, 'html.parser')
         date = timezone.now()
 
         result = self.parser.parse_schedule_item(original_data_parsed, date)
+        self.assertEqual(result['title'], "Test Schedule")
         self.assertEqual(result['start_time'].hour, 21)
         self.assertEqual(result['start_time'].minute, 35)
 
+    def test_get_tcast_channel_ratings(self):
+        """Test to get rating information from schedule of t.cast channels."""
+        rating = self.parser.get_rating("../images/common/icon_15age.gif")
+        self.assertEqual(rating, 15)
+
     def test_get_tcast_schedule(self):
         """Test to get t.cast channel schedule."""
-
+        today = timezone.datetime.today()
         end_date = self.parser.get_channel_schedule("http://www.imtcast.com/screen/program/schedule.jsp")
 
-        self.assertEqual(end_date.day, 28)
-        self.assertEqual(end_date.month, 4)
-
-    def test_get_tcast_next_schedule(self):
-        """Test to get t.cast channel schedule on next week."""
-        end_date = self.parser_next_week.get_channel_schedule("http://www.imtcast.com/screen/program/schedule.jsp")
-
-        self.assertEqual(end_date.day, 28)
-        self.assertEqual(end_date.month, 4)
+        print("Today: " + str(today))
+        print("Next schedule date: " + str(end_date))
+        self.assertNotEqual(today, end_date)
+        self.assertEqual(today < end_date, True)
 
 
 class DeleteLastWeekScheduleTest(TestCase):
