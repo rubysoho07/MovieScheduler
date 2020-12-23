3b678ef08b9599e0a5c855b5b6c01135eebdd609
Yungon Park <hahafree12@gmail.com>
2020-12-23 21:51:07 +0900

    편성표 URL 컬럼 추가

--- scheduler_core/models.py
+++ scheduler_core/models.py
@@ -6,6 +6,7 @@ class BroadcastCompany(models.Model):
     """Information of broadcast company"""
 
     bc_name = models.CharField(max_length=100, unique=True)
+    schedule_url = models.CharField(max_length=400, default=None, null=True)
 
     def __str__(self):
         return self.bc_name
