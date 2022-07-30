3225a447f25b7013ce4e5c55d60a201f0103b5d8
Yungon Park <hahafree12@gmail.com>
2022-07-30 13:43:06 +0900

    Fix url() function to re_path() for Django 4.0

--- movie_scheduler/urls.py
+++ movie_scheduler/urls.py
@@ -1,34 +1,20 @@
-"""movie_scheduler URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.10/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
-"""
-from django.conf.urls import url
+"""movie_scheduler URL Configuration"""
+from django.urls import re_path
 from django.contrib import admin
 from scheduler_core import views
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/$', views.MovieScheduleDAV.as_view(), name='day'),
-    url(r'^$', views.MovieScheduleTAV.as_view(), name='today_schedule'),
-    url(r'^license/$', views.LicenseTemplateView.as_view(), name='license'),
-    url(r'^setting/$', views.BroadcastCompanyDisplaySettingView.as_view(), name='setting'),
+    re_path(r'^admin/', admin.site.urls),
+    re_path(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/$', views.MovieScheduleDAV.as_view(), name='day'),
+    re_path(r'^$', views.MovieScheduleTAV.as_view(), name='today_schedule'),
+    re_path(r'^license/$', views.LicenseTemplateView.as_view(), name='license'),
+    re_path(r'^setting/$', views.BroadcastCompanyDisplaySettingView.as_view(), name='setting'),
 
-    url(r'^broadcast/(?P<pk>\d+)/(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/$',
-        views.BroadcastDailyScheduleDAV.as_view(), name='daily_schedule'),
-    url(r'^broadcast/all/(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/$',
-        views.AllBroadcastDailyScheduleDAV.as_view(), name='daily_schedule'),
+    re_path(r'^broadcast/(?P<pk>\d+)/(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/$',
+            views.BroadcastDailyScheduleDAV.as_view(), name='daily_schedule'),
+    re_path(r'^broadcast/all/(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/$',
+            views.AllBroadcastDailyScheduleDAV.as_view(), name='daily_schedule'),
 
-    url(r'^broadcast/today/(?P<pk>\d+)/$', views.BroadcastTodayScheduleView.as_view(), name='today_schedule'),
-    url(r'^broadcast/today/all/$', views.AllBroadcastTodayScheduleView.as_view(), name='today_all_schedule'),
+    re_path(r'^broadcast/today/(?P<pk>\d+)/$', views.BroadcastTodayScheduleView.as_view(), name='today_schedule'),
+    re_path(r'^broadcast/today/all/$', views.AllBroadcastTodayScheduleView.as_view(), name='today_all_schedule'),
 ]
